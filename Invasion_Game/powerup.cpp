79e32cc64284b176a80507b86334e9c3349b769c
Marcin <marcin27zzz@gmail.com>
2017-05-26 17:08:01 +0200

    fail

--- Invasion_Game/powerup.cpp
+++ Invasion_Game/powerup.cpp
@@ -1,10 +1,18 @@
 #include<cstdlib>
 #include "powerup.h"
-PowerUp::PowerUp(double x, double y, int id)
+PowerUp::PowerUp(double x, double y)
 {
 	setx(x);
 	sety(y);
-	setId(id);
+	//setId(id);
+}
+ThreeShots::ThreeShots(double x, double y)
+	:PowerUp(x, y)
+{
+}
+OneShot::OneShot(double x, double y)
+	: PowerUp(x, y)
+{
 }
 double PowerUp::getx()
 {
@@ -25,50 +33,50 @@ void PowerUp::sety(double y)
 {
 	this->y = y;
 }
-double PowerUp::getId()
+/*double PowerUp::getId()
 {
 	return this->y;
-}
+}*/
 
-void PowerUp::setId(int id)
+/*void PowerUp::setId(int id)
 {
 	this->x = x;
-}
+}*/
 
 void PowerUp::move(double x, double y)
 {
 	this->setx(this->getx() + x);
 	this->sety(this->gety() + y);
 }
-void PowerUpHandler::activate(int i)
+void PowerUp::activate()
 {
-	this->active[i] = true;
+	this->active = true;
 }
-void PowerUpHandler::disactivate(int i)
+void PowerUp::disactivate()
 {
-	this->active[i] = false;
+	this->active = false;
 }
-bool PowerUpHandler::getState(int i)
+bool PowerUp::getState()
 {
-	return this->active[i];
+	return this->active;
 }
 void PowerUpHandler::generatePowerUp(double x, double y)
 {
 	int r = rand() % 20;
 	if ((r < 2) && powerUp[0] == NULL)
 	{
-		powerUp[0] = new PowerUp(x, y);
-		powerUp[0]->setId(0);
+		powerUp[0] = new OneShot(x, y);
+		//powerUp[0]->setId(0);
 	}
 	else if ((r < 4) && powerUp[1] == NULL)
 	{
-		powerUp[1] = new PowerUp(x, y);
-		powerUp[1]->setId(1);
+		powerUp[1] = new ThreeShots(x, y);
+		//powerUp[1]->setId(1);
 	}
 	else if ((r < 6) && powerUp[2] == NULL)
 	{
-		powerUp[2] = new PowerUp(x, y);
-		powerUp[2]->setId(2);
+		powerUp[2] = new ThreeShots(x, y);
+		//powerUp[2]->setId(2);
 	}
 }
 
@@ -88,31 +96,35 @@ void PowerUpHandler::updatePowerUp()
 	}
 }
 
-void PowerUpHandler::powerUpEffect(int id, Player *player, float time)
+void ThreeShots::powerUpEffect(Player *player, float time)
 {
-	if (id == 0)
-	{
-		player->setPlayerProjectile(1);
-	}
-	if (id == 2) player->setPlayerProjectile(3);
-	additionalTime[id] = sf::seconds(time);
-	powerUpClock[id].restart();
-	powerUpTime[id] = powerUpClock[id].getElapsedTime();
-	this->activate(id);
+	player->setPlayerProjectile(3);
+	additionalTime = sf::seconds(time);
+	powerUpClock.restart();
+	powerUpTime = powerUpClock.getElapsedTime();
+	this->activate();
+}
+void OneShot::powerUpEffect(Player *player, float time)
+{
+	player->setPlayerProjectile(1);
+	additionalTime = sf::seconds(time);
+	powerUpClock.restart();
+	powerUpTime = powerUpClock.getElapsedTime();
+	this->activate();
 }
 
 void PowerUpHandler::powerUpEffectUpdate(Player *player)
 {
 	for (int i = 0; i < 3; i++)
 	{
-		powerUpTime[i] = powerUpClock[i].getElapsedTime();
+		powerUpTime[i] = powerUpClock.getElapsedTime();
 		powerUpTime[i] += additionalTime[i];
 
-		if ((powerUpTime[i].asSeconds() > 10) && this->getState(i))
+		if ((powerUpTime[i].asSeconds() > 10) && this->getState())
 		{
 			player->setPlayerProjectile(2);
-			this->disactivate(i);
-			additionalTime[i] = sf::Time::Zero;
+			this->powerUp[i]->disactivate();
+			powerUp[i]->additionalTime = sf::Time::Zero;
 
 		}
 	}
